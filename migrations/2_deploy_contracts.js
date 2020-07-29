678740e34a319f8a5163ad5dec88646428bb1e47
Kevin Cela <kevincela97@gmail.com>
2020-07-29 18:48:30 +0200

    connect truffle with quorum blockchain

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,16 @@ let ImageStorage = artifacts.require("./ImageStorage.sol");
 let GiornaleDeiLavori = artifacts.require("./GiornaleDeiLavori.sol");
 let LogFotogrammetria = artifacts.require("./LogFotogrammetria.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(ImageStorage);
-  deployer.deploy(GiornaleDeiLavori);
-  deployer.deploy(LogFotogrammetria);
+module.exports = async (deployer) => {
+  await deployer.deploy(ImageStorage);
+  await deployer.deploy(GiornaleDeiLavori);
+  await deployer.deploy(LogFotogrammetria);
+  let IS = await ImageStorage.deployed();
+  let GDL = await GiornaleDeiLavori.deployed();
+  let SF = await LogFotogrammetria.deployed();
+  
+  //TODO: salvare gli indirizzi su mongo
+  console.log("INDIRIZZO IMAGESTORAGE: " + IS.address)
+  console.log("INDIRIZZO GIORNALE DEI LAVORI: " + GDL.address)
+  console.log("INDIRIZZO LOG FOTOGRAMMETRIA: " + SF.address)
 };
